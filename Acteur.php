adb561aed790f148a7cae8529994f526c1e85d6f
KrouriB <brice.krouri@gmail.com>
2023-02-09 11:46:42 +0100

    création class Role + fonction permetant d'afficher un objet et ces objet lié a lui dans les 2 sens (Role/Acteur)

--- Acteur.php
+++ Acteur.php
@@ -1,7 +1,32 @@
 <?php
 
-class Acteur extends Personne{
-    private array $roleJouer;
-}
+class Acteur extends Personne
+{
+	private array $roles;
+	private Role $role;
+	private Casting $cast;
 
-?>
+	public function __construct(string $nom, string $prenom, string $sexe, string $laDateNaissance, Role $role)
+	{
+		parent::__construct($nom, $prenom, $sexe, $laDateNaissance);
+		$this->role->ajoutActeur($this);
+	}
+
+	public function ajoutRole(Role $lesRoles)
+	{
+		$this->roles[] = $lesRoles;
+	}
+
+
+	public function afficherRoleDeLActeur()
+	{
+		$display = "";
+		$display .= "Les role ayant été incarer par cette l'acteur ";
+		$display .= parent::get_nom_prenom();
+		$display = " sont : <br>";
+		foreach ($this->roles as $unRole) {
+			$unRole->get_role();
+		}
+		$display = "<br>";
+	}
+}
---
+++ Role.php
@@ -0,0 +1,47 @@
+<?php
+
+class Role
+{
+	private string $role;
+	private array $lesacteurs;
+	private Casting $cast;
+	private Acteur $acteur;
+
+	public function __construct(string $role, Acteur $acteur)
+	{
+		$this->role = $role;
+		$this->acteur->ajoutRole($this);
+	}
+
+	public function set_role(string $role)
+	{
+		$this->role = $role;
+	}
+	public function get_nom()
+	{
+		return $this->role;
+	}
+
+	public function ajoutActeur(Acteur $unActeur)
+	{
+		$this->lesacteurs[] = $unActeur;
+	}
+
+
+	public function afficherActeurDuRole()
+	{
+		$display = "";
+		$display .= "Les acteur ayant jouer ";
+		$display .= $this;
+		$display = " sont : <br>";
+		foreach ($this->lesacteurs as $unActeur) {
+			$unActeur->get_nom_prenom();
+		}
+		$display = "<br>";
+	}
+
+	public function __toString()
+	{
+		return "le role de " . $this->role;
+	}
+}
